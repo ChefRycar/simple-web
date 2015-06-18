32c583d2166141b777b6cbc2e3782bc422493ebf
Nick Rycar <rycar@chef.io>
2015-06-18 07:31:34 -0700

    Adding serverspec tests.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,8 +5,17 @@ describe 'simple-web::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
   
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe file('/var/www/index.html') do
+    it { should be_file }
+    it { should be_mode 644 }
+  end
+
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  describe service('apache2-default') do
+    it { should be_running }
   end
 
 end
